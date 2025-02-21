b3a19148430ce504cbe78d0c9b8f404f25b52fa1
KatiaBarba <katiabarba5@hotmail.com>
2025-02-20 22:02:28 -0600

    Elimina exceso de notas

--- app.js
+++ app.js
@@ -6,27 +6,26 @@ let listaAmigos = [];
 console.log(listaAmigos)
 
 //Implementa una función para agregar amigos
-//que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos
 function agregarAmigo()
 {
-    //Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+    //Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector
     let nombreIngresado = document.getElementById("amigo").value;
-    //Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+    //Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. 
     if (nombreIngresado == '')
     {
         alert("Por favor, inserte un nombre.");
     }
     console.log(nombreIngresado);
-    //Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+    //Actualizar el array de amigos: Añadir el valor al arreglo usando el método.push().
     listaAmigos.push(nombreIngresado);
     //Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
     //limpiarCaja();
     document.getElementById('amigo').value = '';
-
 }
 
+/* NOTAS: 
+Mostrar un nombre sorteado:
+console.log(nombreDeLaLista(index))
 
-// mostrar un nombre sorteado
-// console.log(nombreSorteado(index))
-// saber el tamanio de la lista
-//numerosSorteados.length*/
+Saber el tamanio de la lista:
+nombreDeLaLista.length*/
